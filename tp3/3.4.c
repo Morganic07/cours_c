146b8e418ebeb0a01f22902b3c05e47789aa5681
Morgane <phoenixroyal78@gmail.com>
2026-09-08 12:51:46 +0200

    3.4 alternative

--- tp3/3.4.c
+++ tp3/3.4.c
@@ -24,3 +24,22 @@ int main(){
     return 0;
     
 }
+
+
+
+int alternative(){
+
+    int a = 1;
+    int b = -2;
+
+    if (a*b>0) {
+        printf("le produit des deux nombres est positif\n");
+    }
+    else if (a*b<0) {
+        printf("le produit des deux nombres est négatif\n");
+    }
+    else {
+        printf("le produit des deux nombres est nul\n");
+    }
+
+}
