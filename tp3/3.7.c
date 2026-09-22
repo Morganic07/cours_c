bb8848ade6baceea5f037d627c4aa62de74f656c
Morgane <phoenixroyal78@gmail.com>
2026-09-22 11:07:39 +0200

    3.7

---
+++ tp3/3.7.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+
+int main(){
+
+    int chiffre;
+
+    scanf("%d", &chiffre);
+
+    modulo = chiffre%2;
+
+    if modulo == 0{
+        printf("pair");
+    }
+
+    else{
+        printf("impair");
+    }
+
+
+
+    return 0;
+
+}
