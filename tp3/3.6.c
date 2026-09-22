0e2c2db5c6c5e888242aa3599e473508684d7f5c
Morgane <phoenixroyal78@gmail.com>
2026-09-22 11:05:24 +0200

    3.6

--- tp3/3.6.c
+++ tp3/3.6.c
@@ -2,7 +2,27 @@
 
 
 int main(){
+    float moyenne = 10;
 
+    if (moyenne < 10) {
+        printf("recalé");
+    }
+
+    else if (10 <= moyenne && moyenne < 12) {
+        printf("passable");
+    }
+
+    else if (12 <= moyenne && moyenne < 14){
+        printf(" assez bien");
+    }
+
+    else if (14 <= moyenne && moyenne < 16){
+        printf("bien");
+    }
+
+    else if (16 <= moyenne){
+        printf("tres bien");
+    }
 
     return 0;
 }
