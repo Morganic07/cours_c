095b2770ac9132329a26f51966153e1bd27966f9
Morgane <phoenixroyal78@gmail.com>
2026-09-22 11:12:53 +0200

    3.8

---
+++ tp3/3.8.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+
+int main(){
+    int annee;
+
+    scanf("%d", &annee);
+
+
+    if (annee%4==0 && annee%400==0){
+        printf("bissextile");
+    }
+
+
+    else{
+        printf("non bissextile");
+    }
+
+
+    return 0;
+}
